c1338197a120d5ab85e734983f41a9f455636a5b
Hoang <hoanghtpd05588@fpt.edu.vn>
2023-04-27 23:13:13 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/views/index.html')
 })
