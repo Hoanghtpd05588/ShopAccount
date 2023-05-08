1d3a2345c57d920eeac75127049a42be774e2fce
Hoang <hoanghtpd05588@fpt.edu.vn>
2023-05-08 17:14:24 +0700

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true
+}
+))
+app.use(express.json())
 
 // HTTP logger
 // app.use(morgan('combined'))
--- src/resources/js/routes.js
+++ src/resources/js/routes.js
@@ -3,7 +3,10 @@ module.exports = function (app) {
         res.render('home')
     })
     app.get('/search', (req, res) => {
-        console.log(req.query)
         res.render('search')
+    })
+    app.post('/search', (req, res) => {
+        console.log(req.body)
+        res.send('')
     })
 }
